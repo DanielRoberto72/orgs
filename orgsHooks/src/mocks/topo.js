333c08cf6bae5ae0f748f60f34f4518cb4a4117a
Daniel Roberto Pereira <daniel.roberto@surf.com.br>
2022-07-23 10:11:39 -0300

    feat: adicionando mocks e usando state

---
+++ orgsHooks/src/mocks/topo.js
@@ -0,0 +1,6 @@
+const topo = {
+  boasVindas: 'Olá Daniel,',
+  legenda: 'Encontre os melhores produtores',
+};
+
+export default topo;
--- orgsHooks/src/telas/Home/index.js
+++ orgsHooks/src/telas/Home/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import Topo from './componentes/Topo';
 
 export default function Home() {
-    return (
-      <Topo/>
-    )
+  return <Topo />;
 }
